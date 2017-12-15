5317f385e61f422f718278316b28b5cfea528154
sawl_stone <sawl.py@outloock.com>
2017-12-15 22:42:10 +0200

    valid_time_response

--- berlin_clock/b_clock.py
+++ berlin_clock/b_clock.py
@@ -5,21 +5,21 @@ def valid_time(t):
         m = int(t.split(":")[1])
         s = int(t.split(":")[2])
         if h < 0 or m < 0 or s < 0:
-            return False
+            return "Data value can't be negative"
         if h > 23:
-            return False
+            return "Hour must be lower then 24"
         elif m > 59 or s > 59:
-            return False
+            return "Minute or second must be lower then 60"
         return [s % 2 == 0, h // 5, h % 5, m // 5, m % 5]
     except:
-        return False
+        return "Wrong time format! Use this one hh:mm:ss"
 
 
 def berlin_clock(time):
     print("\n### Berlin Clock ###\n")
     print("-----\nEntered time: {}".format(time))
     vt = valid_time(time)
-    if vt:
+    if type(vt) == list:
         if vt[0]:
             print('Y')
         else:
@@ -36,7 +36,7 @@ def berlin_clock(time):
         print("".join(l_line))
         print(vt[4]*'Y' + (4-vt[4])*'O')
     else:
-        print("Wrong time format! Use this one hh:mm:ss")
+        print(vt)
     print("-----")
 
 
@@ -45,11 +45,11 @@ def simple_berlin_clock(time):
     print("\n\n---Simple version---")
     print("\nEntered time: {}".format(time))
     vt = valid_time(time)
-    if vt:
+    if type(vt) == list:
         for i in vt:
             print(i * '|#|')
     else:
-        print("Wrong time format! Use this one hh:mm:ss")
+        print(vt)
     print("-----")
 
 
